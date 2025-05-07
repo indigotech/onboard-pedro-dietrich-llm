8142ef6a6f13b700fc03dfd24ea0e8fe46179c99
pedro_dietrich <pedro.martins@taqtile.com.br>
2025-05-07 17:03:15 -0300

    Move common logic to a separate file
    The logic for fetching the CLI arguments and the LLM chat model were moved
    to a separate file.

---
+++ scripts/chat_config.py
@@ -0,0 +1,21 @@
+import argparse
+import os
+from langchain.chat_models import init_chat_model
+from langchain_core.language_models import BaseChatModel
+
+def get_arguments() -> argparse.Namespace:
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-v', '--vendor', type=str, choices=['openai', 'groq'], default='openai')
+    parser.add_argument('-s', '--stream', action='store_true', default=False)
+    return parser.parse_args()
+
+def get_chat_model(vendor: str) -> BaseChatModel:
+    print(f'Selected vendor: {vendor}')
+
+    if not os.environ.get(f'{vendor.upper()}_API_KEY'):
+        raise ValueError(f'API key not defined for the vendor {vendor}.')
+
+    if vendor == 'openai':
+        return init_chat_model('gpt-4o-mini', model_provider=vendor)
+    else:
+        return init_chat_model('llama-3.3-70b-versatile', model_provider=vendor)
