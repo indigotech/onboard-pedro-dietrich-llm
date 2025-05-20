098ce11d0576d2d81d8d1106c998053b99b0d55d
pedro_dietrich <pedro.martins@taqtile.com.br>
2025-05-20 15:48:32 -0300

    Fix multi-agent chatbot system
    The multi-agent chatbot system has been updated to fix errors and to comply
    with the onboarding specifications.

--- scripts/06_langgraph_multi_agent.py
+++ scripts/06_langgraph_multi_agent.py
@@ -1,42 +1,30 @@
 from dotenv import load_dotenv
-from langchain_core.tools import InjectedToolCallId
-from langgraph.graph import StateGraph, MessagesState
-from langgraph.graph.state import CompiledStateGraph
-from langgraph.prebuilt import InjectedState
-from langgraph.types import Command
-from typing import Annotated
+from langchain_core.messages import AIMessageChunk, ToolMessageChunk, ToolCall
+from langchain_core.runnables import Runnable
 
 from chat_config import *
 from chat_history import ChatHistory
 from db import init_db
 
 
-def create_handoff_tool(*, agent_name: str, description: str | None = None) -> BaseTool:
+def create_agent_call_tool(agent: CompiledGraph, agent_name: str, description: str | None = None) -> BaseTool:
     name = f'transfer_to_{agent_name}'
-    description = description or f'Ask {agent_name} for help.'
+    description = description or f'Delegate task to the {agent_name}.'
 
     @tool(name, description=description)
-    def handoff_tool(state: Annotated[MessagesState, InjectedState], tool_call_id: Annotated[str, InjectedToolCallId]) -> Command:
-        tool_message = {
-            'role': 'tool',
-            'content': f'{text_colors["cyan2"]}Successfully transfered to {agent_name}.{text_colors["blue2"]}',
-            'name': name,
-            'tool_call_id': tool_call_id
-        }
-        return Command(goto=agent_name, update={**state, 'messages': state['messages'] + [tool_message]}, graph=Command.PARENT)
+    def call_tool(query: str) -> str:
+        response = agent.invoke({'messages': [HumanMessage(content=query)]})
+        message: BaseMessage = response['messages'][-1]
+        return str(message.content)
 
-    return handoff_tool
+    return call_tool
 
 
-def create_system_graph(vendor: str) -> CompiledStateGraph:
+def create_agents(vendor: str) -> tuple[Runnable, dict[str, BaseTool]]:
     llm_model = get_chat_model(vendor)
 
-    assign_to_research_agent = create_handoff_tool(agent_name='research_agent', description='Assign task to the research agent.')
-    assing_to_calculator_agent = create_handoff_tool(agent_name='calculator_agent', description='Assing task to the calculator agent.')
-    assing_to_writer_agent = create_handoff_tool(agent_name='writer_agent', description='Assing task to the writer agent.')
-
-    researcher_agent = create_react_agent(
-        name='researcher_agent',
+    research_agent = create_react_agent(
+        name='research_agent',
         model=llm_model,
         tools=[web_search],
         prompt=(
@@ -64,70 +52,89 @@ def create_system_graph(vendor: str) -> CompiledStateGraph:
         )
     )
 
-    supervisor_agent = create_react_agent(
-        name='supervisor',
-        model=llm_model,
-        tools=[assign_to_research_agent, assing_to_calculator_agent, assing_to_writer_agent],
-        prompt=(
-            'You are a supervisor managing three agents:\n'
-            '- A research agent. Assign research related tasks to this agent.\n'
-            '- A calculator agent. Assing math tasks to this agent.\n'
-            '- A writer agent. Assing text generation tasks to this agent.\n'
-            'Assing work to one agent at a time, do not call agents in parallel.\n'
-            'Do not do any work yourself.\n'
-            'Never write the responses to the user messages, assign the writer agent to to that, always.'
-        )
+    call_research_agent = create_agent_call_tool(
+        research_agent,
+        agent_name='research_agent',
+        description='Assign task to the research agent.'
     )
-    #supervisor_agent = llm_model.bind_tools([assign_to_research_agent, assing_to_calculator_agent, assing_to_writer_agent])
+    call_calculator_agent = create_agent_call_tool(
+        calculator_agent,
+        agent_name='calculator_agent',
+        description='Assing task to the calculator agent. Assign all math operations to the calculator agent, always.'
+    )
+    call_writer_agent = create_agent_call_tool(
+        writer_agent,
+        agent_name='writer_agent',
+        description='Assing task to the writer agent. Always use the writer agent to write the responses.'
+    )
 
-    graph = StateGraph(MessagesState)
-
-    graph.add_node(supervisor_agent)
-    graph.add_node(researcher_agent)
-    graph.add_node(calculator_agent)
-    graph.add_node(writer_agent)
-
-    graph.set_entry_point('supervisor')
-    graph.add_edge('researcher_agent', 'supervisor')
-    graph.add_edge('calculator_agent', 'supervisor')
-    graph.add_edge('writer_agent', 'supervisor')
-
-    return graph.compile()
-
-
-def query_llm(system_graph: CompiledStateGraph, chat_history: ChatHistory) -> None:
-    output = system_graph.invoke({'messages': chat_history.messages})
-    new_messages = output['messages'][len(chat_history.messages):]
-    for m in new_messages:
-        if isinstance(m, BaseMessage):
-            chat_history.add_message(m)
-            if m.content: print(f'{m.content}')
-
-def query_llm_stream(system_graph: CompiledStateGraph, chat_history: ChatHistory) -> None:
-    for chunk in system_graph.stream({'messages': chat_history.messages}):
-        try:
-            new_messages = chunk['supervisor']['messages'][len(chat_history.messages):]
-        except:
-            new_messages = []
-
-        for m in new_messages:
-            if isinstance(m, BaseMessage):
-                if m.content: print(m.content, flush=True)
-                chat_history.add_message(m)
-            elif isinstance(m, dict) and m['role'] == 'tool':
-                print(f'{text_colors["violet2"]}Tool: {m["content"]}{text_colors["blue2"]}', flush=True)
+    supervisor_runnable = llm_model.bind_tools([call_research_agent, call_calculator_agent, call_writer_agent])
+    return supervisor_runnable, {
+        'transfer_to_research_agent': call_research_agent,
+        'transfer_to_calculator_agent': call_calculator_agent,
+        'transfer_to_writer_agent': call_writer_agent
+    }
 
 
-def chat(system_graph: CompiledStateGraph, chat_history: ChatHistory, stream: bool) -> None:
+def query_llm(chat_history: ChatHistory, chain: Runnable) -> list[ToolCall]:
+    output_message = chain.invoke(chat_history.messages)
+    tool_calls: list[ToolCall] = []
+
+    if isinstance(output_message, BaseMessage):
+        chat_history.add_message(output_message)
+    if isinstance(output_message, AIMessage):
+        ai_response = str(output_message.content)
+        if ai_response:
+            print(f'{text_colors["blue"]}{ai_response}\n')
+        tool_calls = output_message.tool_calls
+
+    return tool_calls
+
+def query_llm_stream(chat_history: ChatHistory, chain: Runnable) -> list[ToolCall]:
+    ai_response = ''
+    gathered = AIMessageChunk(content='')
+    is_first = True
+
+    for chunk in chain.stream(chat_history.messages):
+        ai_response += str(chunk.content)
+        if is_first:
+            print(text_colors['blue'], end='', flush=True)
+            gathered = chunk
+            is_first = False
+        else:
+            gathered = gathered + chunk
+        print(chunk.content, end='', flush=True)
+
+    if gathered.content: print('\n')
+
+    tool_calls = gathered.tool_calls
+
+    if isinstance(gathered, AIMessageChunk):
+        chat_history.add_message(AIMessage(content=gathered.content, tool_calls=gathered.tool_calls))
+    elif isinstance(gathered, ToolMessageChunk):
+        chat_history.add_message(ToolMessage(content=gathered.content))
+
+    return tool_calls
+
+
+def chat(chat_history: ChatHistory, chain: Runnable, subagent_calls: dict[str, BaseTool], stream: bool) -> None:
     user_input = input(f'\n{text_colors["green2"]}User ("quit" to exit): ')
-    print()
+    print(flush=True)
     while user_input != 'quit' and user_input != 'exit':
         chat_history.add_message(HumanMessage(content=user_input))
+        agent_loop_count = 0
+        while agent_loop_count < 5:
+            tool_calls = query_llm_stream(chat_history, chain) if stream else query_llm(chat_history, chain)
+            if len(tool_calls) == 0: break
 
-        print(text_colors['blue2'], end='', flush=True)
-        query_llm_stream(system_graph, chat_history) if stream else query_llm(system_graph, chat_history)
+            for tool_call in tool_calls:
+                print(f'{text_colors["violet2"]}Tool call: {tool_call["name"]}\n')
+                selected_tool = subagent_calls[tool_call['name']]
+                tool_response: ToolMessage = selected_tool.invoke(tool_call)
+                chat_history.add_message(tool_response)
+            agent_loop_count += 1
 
-        user_input = input(f'\n{text_colors["green2"]}User: ')
+        user_input = input(f'{text_colors["green2"]}User: ')
         print()
 
 
@@ -143,9 +150,9 @@ def main():
 
     chat_history = ChatHistory(args.chatid, conn)
 
-    system_graph = create_system_graph(args.vendor)
+    supervisor, subagent_calls = create_agents(args.vendor)
 
-    chat(system_graph, chat_history, args.stream)
+    chat(chat_history, supervisor, subagent_calls, args.stream)
     chat_history.save_messages()
 
     conn.close()
